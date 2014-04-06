55c3d774d77bc28c7e82b3cdd1dfb044c34688ea
Essam Joubori <essam.joubori@gmail.com>
2014-04-06 16:52:06 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuneiform::Application.config.secret_key_base = '8864f4ea1fff74467559f75d4bf0fc0a64dbc63d1418950b2f962ad6e9cb5a0abc58973dfbd170a87636a2635684a158f5d77c059af6fd36d382788deb25a6b4'
